bffbaa2eba7d97db36478844a1f3c89707ca63c7
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-06-12 17:01:08 +0000

    1. In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- create users table with specific requirements
+CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        name VARCHAR(255),
+        country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
