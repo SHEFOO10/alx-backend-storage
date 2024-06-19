07f97b736ad5d0bae4ab5eb2940eeacadcbe46c3
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-06-19 17:39:01 +0000

    Add flushdb

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,10 +7,11 @@ from typing import Union
 
 class Cache:
     """ Cache class """
-    def __init__(self):
+    def __init__(self) -> None:
         self._redis = redis.Redis()
+        self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, float, int]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         store the input data in Redis using the random key
         and return the key
